71bc62057161f9c714d9f1a5ccc24ed25faf741e
Predrag Njegovanovic <djaps94@gmail.com>
2019-03-31 01:19:25 +0100

    Run environment in parallel

--- parallel_env_wrapper.py
+++ parallel_env_wrapper.py
@@ -2,13 +2,19 @@ from multiprocessing import Process, Pipe
 from obstacle_tower_env import ObstacleTowerEnv
 
 
-def start_environment(connection, env_path, retro, realtime_mode):
-    obstacle_tower = ObstacleTowerEnv(env_path, retro=retro, realtime_mode=realtime_mode)
+def start_environment(connection, worker_id, env_path, retro, realtime_mode):
+    obstacle_tower = ObstacleTowerEnv(env_path,
+                                      worker_id=worker_id,
+                                      retro=retro,
+                                      timeout_wait=90,
+                                      realtime_mode=False)
 
     while True:
-        command, data = connection.recv()
+        command, action = connection.recv()
+        if command == 'sample':
+            connection.send(obstacle_tower.action_space.sample())
         if command == 'step':
-            state, reward, done, info = obstacle_tower.step(data)
+            state, reward, done, info = obstacle_tower.step(action)
             if done:
                 state = obstacle_tower.reset()
             connection.send((state, reward, info))
@@ -30,24 +36,32 @@ class ParallelEnvironmentWrapper:
 
     def start_parallel_execution(self):
         self.processes = [Process(target=start_environment,
-                                  args=(child, self.env_path,
+                                  args=(child, worker_id, self.env_path,
                                         self.retro, self.realtime_mode),
-                                  daemon=True) for child in self.child_connection]
+                                  daemon=True)
+                                  for worker_id, child in enumerate(self.child_connections)]
 
         for process in self.processes:
             process.start()
+
+    def sample(self):
+        [parent.send(('sample', None)) for parent in self.parent_connections]
+        samples = [parent.recv() for parent in self.parent_connections]
+        return samples
 
     def step(self, actions):
         for action, parent in zip(actions, self.parent_connections):
             parent.send(('step', action))
 
         results = [parent.recv() for parent in self.parent_connections]
+        # return as torch.Tensor
         return results
 
     def reset(self):
         [parent.send(('reset', None)) for parent in self.parent_connections]
 
         states = [parent.recv() for parent in self.parent_connections]
+        # return as torch.Tensor
         return states
 
     def close(self):
--- runner.py
+++ runner.py
@@ -2,11 +2,17 @@ import os
 import multiprocessing
 import definitions
 
-from env_wrapper import ParallelWrapper
+from gym import spaces
+from obstacle_tower_env import ObstacleTowerEnv
+from parallel_env_wrapper import ParallelEnvironmentWrapper
 
 NUM_THREADS = multiprocessing.cpu_count()
 
 if __name__ == "__main__":
     obs_env_path = os.path.join(definitions.OBSTACLE_TOWER_DIR, 'obstacletower')
-    wrapper = ParallelWrapper(obs_env_path)
-    parallel_env = wrapper.wrapped_environment(1)
+    wrapper = ParallelEnvironmentWrapper(obs_env_path, NUM_THREADS)
+    wrapper.start_parallel_execution()
+    for i < range(10):
+        samples = wrapper.sample()
+        results = wrapper.step(samples)
+        print(results)
